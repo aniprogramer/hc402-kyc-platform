3c31fbaab37fd592ede023394b322ec7a8a34f7e
aniprogramer <aniketde66@gmail.com>
2026-02-15 06:47:29 +0000

    updated docker files and initialized ngrok

--- backend/main.py
+++ backend/main.py
@@ -21,7 +21,7 @@ KYCSession.metadata.create_all(bind=engine)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
